fab23390e0c9cc7a196aefad303a78a1d4b20a37
Terunami <nobooknolife16@yahoo.co.jp>
2020-05-13 19:40:28 +0900

    設定ファイル編集

---
+++ project/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\takahiro\desktop\python_web_app\django-tutorial\project\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
